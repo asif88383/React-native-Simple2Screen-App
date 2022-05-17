c02ee3f26a3ecac0ef6076e8a54b8df1a1f70937
Muhammad Asif <asif88383@gmail.com>
2022-05-17 12:58:28 +0500

    first commit

--- Simple2ScreenApp/App.js
+++ Simple2ScreenApp/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Home from './Screens/Home';
+import Screen from './Screens/Screen1';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SafeAreaView>
+        <ScrollView>
+          <Home/>
+          <Screen/>
+          <StatusBar style="auto" />
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
--- Simple2ScreenApp/Screens/Screen1.jsx
+++ Simple2ScreenApp/Screens/Screen1.jsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native-paper'
 
 const Screen1 = () => {
   return (
-    <View>
-      <Text>Screen1</Text>
+    <View style={{marginTop:50, backgroundColor: "grey"}}>
+      <Text>Home</Text>
+
+      <Button
+        color='white'
+        style={{margin: 30, backgroundColor: '#00bcd4', borderRadius: 30}}
+        >Click Me</Button>
     </View>
   )
 }
