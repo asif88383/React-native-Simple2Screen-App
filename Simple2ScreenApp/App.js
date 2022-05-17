bbd32b82587f0a4be76c017942b261f07dd626f1
Muhammad Asif <asif88383@gmail.com>
2022-05-17 13:33:06 +0500

    first commit

--- Simple2ScreenApp/App.js
+++ Simple2ScreenApp/App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeAppEventEmitter, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
-import Home from './Screens/Home';
-import Screen from './Screens/Screen1';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Screen1 from './Screens/Screen1';
+import {
+  StyleSheet,
+} from "react-native";
+import Home from "./Screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Register from "./Screens/Register";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,18 +12,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Screen" component={Screen1} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerStyle: { backgroundColor: "lightcyan" } }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- Simple2ScreenApp/Screens/Home.jsx
+++ Simple2ScreenApp/Screens/Home.jsx
@@ -1,19 +1,26 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native-paper'
+import SafeAreaViewAndroid from '../Components/SafeAreaViewAndroid'
 
-const Home = ({navigation}) => {
+const Main = ({navigation, title}) => {
   return (
     <View style={{marginTop:50, backgroundColor: "grey"}}>
-      <Text>Home</Text>
+      <Text>{title}</Text>
 
       <Button
         color='white'
         style={{margin: 30, backgroundColor: '#00bcd4', borderRadius: 30}}
-        onPress={() => navigation.navigate('Screen')}
-        >Go to Screen1</Button>
+        onPress={() => navigation.navigate('Register')}
+        >Register</Button>
     </View>
   )
+}
+
+const Home = ({navigation}) => {
+    return (
+        <SafeAreaViewAndroid Component={Main}  navigate={navigation} title="Home"/>
+    )
 }
 
 export default Home
