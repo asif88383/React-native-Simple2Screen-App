506bc9e9766e4ec0d0d3de3b51ec72328a6c72b5
Muhammad Asif <asif88383@gmail.com>
2022-05-17 15:46:35 +0500

    first commit

--- Simple2ScreenApp/Components/SafeAreaViewAndroid.jsx
+++ Simple2ScreenApp/Components/SafeAreaViewAndroid.jsx
@@ -1,14 +1,20 @@
-import { View, Platform, StatusBar, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Platform, StatusBar, SafeAreaView } from "react-native";
+import React from "react";
 
-const SafeAreaViewAndroid = ({Component, ...rest}) => {
+const SafeAreaViewAndroid = ({ Component, ...rest }) => {
   return (
-    <View style={{paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0}}>
+    <View
+      style={{
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        backgroundColor: "white",
+        flex: 1
+      }}
+    >
       <SafeAreaView>
-        <Component {...rest}/>
+        <Component {...rest} />
       </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
-export default SafeAreaViewAndroid
+export default SafeAreaViewAndroid;
--- Simple2ScreenApp/Screens/Home.jsx
+++ Simple2ScreenApp/Screens/Home.jsx
@@ -1,48 +1,74 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Button, Appbar, Avatar } from "react-native-paper";
+import { Button, Appbar, Avatar, Headline } from "react-native-paper";
 import SafeAreaViewAndroid from "../Components/SafeAreaViewAndroid";
 
 const Main = ({ navigation, title }) => {
   return (
-    <View style={{ marginTop: 50, backgroundColor: "grey" }}>
+    <View>
       {/* <Text>{title}</Text> */}
       <Appbar>
-        <Appbar.Action icon="heart" onPress={() => navigation.navigate("Home")} />
+        <Appbar.Action
+          icon="heart"
+          onPress={() => navigation.navigate("Home")}
+        />
         <Appbar.Content title="Home" />
-        <Appbar.Action icon="account-circle" onPress={() => navigation.navigate("Register")} />
-        <Appbar.Action icon="camera" onPress={() => navigation.navigate("Camera")} />
+        <Appbar.Action
+          icon="account-circle"
+          onPress={() => navigation.navigate("Register")}
+        />
+        <Appbar.Action
+          icon="camera"
+          onPress={() => navigation.navigate("Camera")}
+        />
       </Appbar>
 
       <Avatar.Image
         source={{
-          uri:"https://www.computerhope.com/jargon/r/random-dice.jpg"
+          uri: "https://www.computerhope.com/jargon/r/random-dice.jpg",
         }}
         size={160}
         style={{ marginTop: 50, alignSelf: "center" }}
       />
       <Button
         color="white"
-        style={{ margin: 30, backgroundColor: "#00bcd4", borderRadius: 30, marginHorizontal: 50 }}
+        style={{
+          margin: 30,
+          backgroundColor: "#00bcd4",
+          borderRadius: 30,
+          marginHorizontal: 50,
+        }}
         onPress={() => navigation.navigate("Register")}
       >
         Register
       </Button>
 
-      <Button
-        color="white"
-        style={{ margin: 50, backgroundColor: "#00bcd4", borderRadius: 30 }}
-        onPress={() => navigation.navigate("Screen1")}
+      <Headline
+        style={{
+          textAlign: "center",
+          marginTop: 20,
+        }}
       >
-        Screen1
-      </Button>
+        About Me
+      </Headline>
+      <Text style={{ alignSelf: "center", textAlign: "center", marginTop: 10, width: "65%" }}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum doloribus
+        reiciendis repellat dicta inventore architecto quaerat, beatae facere
+        voluptatibus eaque libero modi perspiciatis itaque impedit odio quos
+        porro. Fugit voluptatibus perspiciatis fuga id at temporibus nam maiores
+        excepturi rerum.
+      </Text>
     </View>
   );
 };
 
 const Home = ({ navigation }) => {
   return (
-    <SafeAreaViewAndroid Component={Main} navigation={navigation} title="Home" />
+    <SafeAreaViewAndroid
+      Component={Main}
+      navigation={navigation}
+      title="Home"
+    />
   );
 };
 
